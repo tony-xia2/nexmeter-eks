098aa89ef03c920b51cddbbe80369e4d3625d0b8
Tony Xia <xiatian199409@gmail.com>
2024-05-15 11:25:17 +0800

    feat: remove unused resources
    Refactors code to only output what is needed.

--- src/main/java/com/nexmeter/App.java
+++ src/main/java/com/nexmeter/App.java
@@ -1,29 +1,23 @@
 package com.nexmeter;
 
-
 import com.pulumi.Context;
 import com.pulumi.Pulumi;
 import com.pulumi.aws.eks.Cluster;
-import com.pulumi.kubernetes.Provider;
-import com.pulumi.kubernetes.ProviderArgs;
-import com.pulumi.kubernetes.helm.v3.Release;
-import com.pulumi.kubernetes.helm.v3.ReleaseArgs;
+import com.pulumi.aws.eks.ClusterArgs;
+import com.pulumi.aws.eks.outputs.ClusterCertificateAuthority;
 
 public class App {
     public static void main(String[] args) {
         Pulumi.run(App::stack);
     }
 
-    private static void stack(Context ctx) {
-        final var cluster = new Cluster("eks-cluster");
-        ctx.export("kubeconfig", cluster.accessConfig());
-        Provider k8sProvider = new Provider("k8sProvider", ProviderArgs.builder().kubeconfig(cluster.accessConfig().toString()).build());
-        new Release("strimziKafkaOperator", ReleaseArgs.builder()
-                .atomic(true)
-                .chart("strimzi-kafka-operator")
-                .cleanupOnFail(true)
-                .createNamespace(true)
-                .dependencyUpdate(true)
+    public static void stack(Context ctx) {
+        // Create a new EKS cluster
+        var cluster = new Cluster("nexmeter", ClusterArgs.builder()
+                .name("nexmeter-eks")
                 .build());
+        ctx.export("endpoint", cluster.endpoint());
+        ctx.export("kubeconfig-certificate-authority-data", cluster.certificateAuthority()
+                .applyValue(ClusterCertificateAuthority::data));
     }
 }
